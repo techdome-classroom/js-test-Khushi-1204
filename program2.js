b59987c5e5208de77d105ec026e2de4023444368
Khushi Parmar <142445974+Khushi-1204@users.noreply.github.com>
2024-05-09 13:22:14 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,30 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
-  };
-  
-  module.exports = decodeTheRing;
+      let sIndex = 0; // Index for iterating over the secret message
+      let pIndex = 0; // Index for iterating over the decoder key
+    
+      while (sIndex < s.length && pIndex < p.length) {
+        if (p[pIndex] === '*') {
+    
+          while (sIndex < s.length && pIndex < p.length && p[pIndex] === '*') {
+            pIndex++;
+            if (pIndex < p.length && p[pIndex] !== '?' && p[pIndex] !== s[sIndex]) {
+              
+              sIndex++;
+            }
+          }
+        } else if (p[pIndex] === '?' || p[pIndex] === s[sIndex]) {
+      
+          sIndex++;
+          pIndex++;
+        } else {
+      
+          return false;
+        }
+      }
+    
+      // If we have iterated through the entire decoder key and secret message without any mismatches, return true
+      return sIndex === s.length && pIndex === p.length;
+    };
+    
+    module.exports = decodeTheRing;
+    
